e34005d6ab3cb09df365c6d01e220a9f6ed7e7d9
Saeed Ghofrani <92137891+saeedghofrani@users.noreply.github.com>
2024-04-23 14:13:13 +0330

    file => change host

--- src/config/database/database.service.ts
+++ src/config/database/database.service.ts
@@ -4,12 +4,12 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DatabaseService {
-    
+
     constructor(private configService: ConfigService) {}
-    
+
     async getConnection() {
         const connection = await createConnection({
-            host: '0.0.0.0',
+            host: '127.0.0.1',
             user: 'root',
             password: '09211953839',
             database: 'uploader',
